f2f5f163eb5bca7c5fe648997578310d29ce637d
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2023-11-05 21:29:15 -0300

    Change user keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,30 +1,40 @@
+------------------------------------------------------------------------------
+--
+-- Quality of life variables
+--
+------------------------------------------------------------------------------
+
 local opts = { noremap = true, silent = true }
-
 local term_opts = { silent = true }
+local keymap = vim.keymap.set
 
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
---Remap space as leader key
+-- Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+------------------------------------------------------------------------------
+--
 -- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+--
+--   normal_mode = "n", insert_mode = "i", visual_mode = "v",
+--   visual_block_mode = "x", term_mode = "t", command_mode = "c",
+--
+------------------------------------------------------------------------------
 
--- Normal --
+------------------------------------------------------------------------------
+--
+-- Normal mode remaps
+--
+------------------------------------------------------------------------------
+
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Open netrw
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
@@ -37,11 +47,19 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Insert --
--- Press jk fast to enter
--- keymap("i", "jk", "<ESC>", opts)
+-- Navigate tabs
+keymap('n', '<Tab>', ':tabnext<CR>', opts)
+keymap('n', '<S-Tab>', ':tabprevious<CR>', opts)
 
--- Visual --
+-- Open a new terminal pane
+keymap('n', '<leader>t', ':split term://$SHELL<CR>', opts)
+
+------------------------------------------------------------------------------
+--
+-- Visual mode remaps
+--
+------------------------------------------------------------------------------
+
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
@@ -51,14 +69,22 @@ keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
--- Visual Block --
+------------------------------------------------------------------------------
+--
+-- Visual Block mode remaps
+--
+------------------------------------------------------------------------------
+
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal --
+------------------------------------------------------------------------------
+--
+-- Terminal mode remaps
+--
+------------------------------------------------------------------------------
+
 -- Better terminal navigation
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
